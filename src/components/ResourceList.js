b34c64b038f2dbae663ac4155dd6a0726bfc1858
Andrea Diotallevi <andrea.diotallevi@outlook.com>
2020-04-30 09:40:26 +0100

    Code re-use with hooks

--- src/components/ResourceList.js
+++ src/components/ResourceList.js
@@ -1,36 +1,13 @@
-import React, { useState, useEffect } from "react";
-import axios from "axios";
-import { render } from "@testing-library/react";
+import React from "react";
+import useResources from "./useResources";
 
 const ResouceList = ({ resource }) => {
-  const [resources, setResources] = useState([]);
-
-  // const fetchResource = async (resource) => {
-  //   const response = await axios.get(
-  //     `http://jsonplaceholder.typicode.com/${resource}`
-  //   );
-
-  //   setResources(response.data);
-  // };
-
-  // useEffect(() => {
-  //   fetchResource(resource);
-  // }, [resource]);
-
-  useEffect(() => {
-    (async (resource) => {
-      const response = await axios.get(
-        `http://jsonplaceholder.typicode.com/${resource}`
-      );
-
-      setResources(response.data);
-    })(resource);
-  }, [resource]);
+  const resources = useResources(resource);
 
   return (
     <ul>
       {resources.map((record) => (
-        <li key={render.id}>{record.title}</li>
+        <li key={record.id}>{record.title}</li>
       ))}
     </ul>
   );
