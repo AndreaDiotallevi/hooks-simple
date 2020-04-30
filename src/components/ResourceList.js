fbab50aa2b31731f22a13340b191d86ce86dd600
Andrea Diotallevi <andrea.diotallevi@outlook.com>
2020-04-30 09:07:54 +0100

    Refactor ResourceList component

---
+++ src/components/ResourceList.js
@@ -0,0 +1,22 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+
+const ResouceList = ({ resource }) => {
+  const [resources, setResources] = useState([]);
+
+  const fetchResource = async (resource) => {
+    const response = await axios.get(
+      `http://jsonplaceholder.typicode.com/${resource}`
+    );
+
+    setResources(response.data);
+  };
+
+  useEffect(() => {
+    fetchResource(resource);
+  }, [resource]);
+
+  return <div>{resources.length}</div>;
+};
+
+export default ResouceList;
